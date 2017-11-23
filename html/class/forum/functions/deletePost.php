b5b1b8c4057ca21de9e2ac0e3ea8099983768042
zabaski <zabaski@gmail.com>
2017-11-23 10:57:24 -0600

    Add delete post function

--- html/class/forum/functions/deletePost.php
+++ html/class/forum/functions/deletePost.php
@@ -13,9 +13,34 @@
 		2 = Idle Timeout
 */
 
+session_start();
 
+//check idle time
+if(($_SESSION['idle'] + 600) < time()){
+	unset($_SESSION['username']);
+	unset($_SESSION['idle']);
+	echo "2";
+	exit();
+}
 
+$mysqli = new mysqli("localhost", "UpdateOnly", "system", "LSU-ACE");
+if($mysqli->connect_errno){
+	echo "0";
+	exit();
+}
 
+$id = $_POST['ID'];
+$pid = $_POST['Pid'];
 
+if(isset($Pid) && isset($ID)){
+	$sql = "UPDATE ForumPost SET Body = 'Deleted', Anonymous = 'true' WHERE Pid = '$pid'";
+	if($mysqli->query($sql))
+		echo "1";
+	else
+		echo "0";
+}
+$mysqli->close();
+
+exit();
 
 ?>
