4318c7c908ab03867cb8bc9946cac3e423500d3d
zabaski <zabaski@gmail.com>
2017-11-23 12:09:43 -0600

    Add variables, constraints and insert method

--- html/class/forum/functions/makeThread.php
+++ html/class/forum/functions/makeThread.php
@@ -16,9 +16,41 @@
 		3 = Title Constraint Error
 		4 = Body Constraint Error
 */
+session_start();
 
+$mysqli = new mysqli("localhost", "Scheduler", "system", "LSU-ACE");
+if($mysqli->connect_errno){
+	echo "0";
+	exit();
+}
 
+$id = $_POST['ID'];
+$title = $_POST['Title'];
+$body = $_POST['Body'];
+$anon = $_POST['Anon'];
 
+//check title constraints
+if(strlen($title) > 40 || strlen($title) <1){
+	echo "3";
+	exit();
+}
 
+//check body constraints
+if(strlen($body) > 400 || strlen($body) <1){
+	echo "4";
+	exit();
+}
 
+//need clarification on Thread table
+if(isset($id) && isset($title) && isset($body) && isset($anon)){
+	$sql = "INSERT ForumPost (Cid, Tid, Pid, Title)
+	VALUES ('', '$id', '', '$title')";
+	if($mysqli->query($sql))
+		echo "1";
+	else
+		echo "0";
+}
+$mysqli->close();
+
+exit();
 ?>
